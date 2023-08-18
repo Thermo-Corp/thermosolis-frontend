c7095ecfd9af43907ff1921a659a1801d1d44d98
Idriss <idrissbenguezzou@gmail.com>
2023-08-18 18:01:32 +0200

    refactor: square container to rectangle

--- thermosolis-frontend/src/pages/Home/Home.jsx
+++ thermosolis-frontend/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 import TopLevel from '../../components/TopLevel/TopLevel';
 import Banner from '../../components/Banner/Banner';
-import SquareContainer from '../../components/Container/Square/SquareContainer';
+import RectangleContainer from '../../components/Container/Square/RectangleContainer';
 
 function Home() {
     return (
@@ -14,15 +14,15 @@ function Home() {
             <Banner />
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "0 10%" }}>
 
-                <SquareContainer backgroundColor="var(--primary-color)" dimension="235px">
+                <RectangleContainer backgroundColor="var(--primary-color)" >
                     <span style={{ fontSize: "100px", fontWeight: "600", lineHeight: "84px", color: "#EBECEE" }}>147</span>
                     <p style={{ color: "#EBECEE", fontSize: "22px", fontWeight: "400", lineHeight: "22px" }}>Projects Completed</p>
-                </SquareContainer>
+                </RectangleContainer>
 
-                <SquareContainer backgroundColor="var(--secondary-color)" dimension="235px">
+                <RectangleContainer backgroundColor="var(--secondary-color)" >
                     <span style={{ fontSize: "100px", fontWeight: "600", lineHeight: "84px", color: "#EBECEE" }}>25+</span>
                     <p style={{ color: "#EBECEE", fontSize: "22px", fontWeight: "400", lineHeight: "22px" }}>Years of Experience</p>
-                </SquareContainer>
+                </RectangleContainer>
             </div>
         </div>
     );
